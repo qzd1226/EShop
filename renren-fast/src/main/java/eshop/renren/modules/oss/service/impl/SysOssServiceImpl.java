005491bdab897cc0c55a2d002403b41e60204465
qzd1226 <690160142@qq.com>
2023-03-09 17:41:49 +0800

    人人部分的重命名

---
+++ renren-fast/src/main/java/eshop/renren/modules/oss/service/impl/SysOssServiceImpl.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package eshop.renren.modules.oss.service.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import eshop.renren.common.utils.PageUtils;
+import eshop.renren.common.utils.Query;
+import eshop.renren.modules.oss.entity.SysOssEntity;
+import eshop.renren.modules.oss.service.SysOssService;
+import eshop.renren.modules.oss.dao.SysOssDao;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+
+@Service("sysOssService")
+public class SysOssServiceImpl extends ServiceImpl<SysOssDao, SysOssEntity> implements SysOssService {
+
+	@Override
+	public PageUtils queryPage(Map<String, Object> params) {
+		IPage<SysOssEntity> page = this.page(
+			new Query<SysOssEntity>().getPage(params)
+		);
+
+		return new PageUtils(page);
+	}
+	
+}
